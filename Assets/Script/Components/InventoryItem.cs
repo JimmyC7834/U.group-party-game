de41eff24f6f38bb33f7a92b59cf048f14487460
JimmyC7834 <c7834@uw.edu>
2023-09-21 01:05:09 -0700

    constructed new factory with new componenets

--- Assets/Script/Components/InventoryItem.cs
+++ Assets/Script/Components/InventoryItem.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using UnityEngine;
 
 namespace Game
@@ -12,23 +15,40 @@ namespace Game
         Crystal,
     }
 
-    public abstract class InventoryItem : MonoBehaviour
+    public class InventoryItem : MonoBehaviour
     {
-        [SerializeField] private HashSet<InventoryItemTag> _tags;
+        private HashSet<InventoryItemTag> _tags = new HashSet<InventoryItemTag>();
+        [SerializeField] private InventoryItemTag[] _itemTags;
 
-        private bool AddTag(InventoryItemTag tag)
+        private void Awake()
+        {
+            foreach (InventoryItemTag tag in _itemTags)
+                AddTag(tag);
+        }
+
+        public bool AddTag(InventoryItemTag tag)
         {
             return _tags.Add(tag);
         }
 
-        private bool RemoveTag(InventoryItemTag tag)
+        public bool RemoveTag(InventoryItemTag tag)
         {
             return _tags.Remove(tag);
         }
 
-        private bool ContainsTag(InventoryItemTag tag)
+        public bool ContainsTag(InventoryItemTag tag)
         {
             return _tags.Contains(tag);
+        }
+
+        public bool Any(HashSet<InventoryItemTag> tags)
+        {
+            return _tags.Intersect(tags).Count() > 0;
+        }
+
+        public InventoryItemTag[] GetTags()
+        {
+            return _tags.ToArray();
         }
     }
 }
